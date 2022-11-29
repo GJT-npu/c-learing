20ba3a193ee8b2dd816df980388b3a46b3817085
gitnpu <gjt_npu@163.com>
2022-11-29 23:08:22 +0800

    函数方法二分查找 函数递归

--- test_11_26/test_11_26/test_hanshu.c
+++ test_11_26/test_11_26/test_hanshu.c
@@ -32,43 +32,43 @@
 //}
 
 //二分查找
-int binary_search(int a[ ] , int k , int s)
-{
-	int left = 0;
-	int right = s - 1;
-	
-	while(left<=right)
-	{
-		int mid = (left + right) / 2;
-		if (a[mid] > k)
-		{
-			right -= 1;
-		}
-		else if (a[mid] < k)
-		{
-			left += 1;
-		}
-		else
-			return mid;
-	}
-	return -1;
-}
-
-int main()
-{
-	int arr[ ] = { 1,2,3,4,5,6,7,8,9,10 };
-	int key = 7;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	//找到了，返回下标
-	//没找到，返回-1
-	int ret = binary_search(arr , key ,sz );
-	if (ret == -1)
-		printf("没找到\n");
-	else
-		printf("找到了，下标是：%d\n" , ret);
-
-	return 0;
-}
+//int binary_search(int a[ ] , int k , int s)
+//{
+//	int left = 0;
+//	int right = s - 1;
+//	
+//	while(left<=right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (a[mid] > k)
+//		{
+//			right -= 1;
+//		}
+//		else if (a[mid] < k)
+//		{
+//			left += 1;
+//		}
+//		else
+//			return mid;
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	int arr[ ] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int key = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//找到了，返回下标
+//	//没找到，返回-1
+//	int ret = binary_search(arr , key ,sz );
+//	if (ret == -1)
+//		printf("没找到\n");
+//	else
+//		printf("找到了，下标是：%d\n" , ret);
+//
+//	return 0;
+//}
 
 
 
